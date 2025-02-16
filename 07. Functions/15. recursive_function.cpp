d78d2b157b41d4df75af91a7b555a393c8eecae8
yosefm17 <yosefmulugeta23@gmail.com>
2025-02-16 13:24:04 +0300

    updated

--- 07. Functions/15. recursive_function.cpp
+++ 07. Functions/15. recursive_function.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 using namespace std;
 
-void fun(int n) // Prints the int passed to the function till the value of that int is 1 or > 0.
+void fun(int num) // Prints the int passed to the function till the value of that int is 1 or > 0.
 {
-    if (n > 0) // Starts with 5, checks if 5 > 0, yes then prints 5, then calls for fun(4)..again checks and goes till 1.
+    if (num > 0) // Starts with 5, checks if 5 > 0, yes then prints 5, then calls for fun(4)..again checks and goes till 1.
     {
-        cout << n << endl;
-        fun(n - 1);
+        cout << num << endl;
+        fun(num - 1);
     }
 }
 
